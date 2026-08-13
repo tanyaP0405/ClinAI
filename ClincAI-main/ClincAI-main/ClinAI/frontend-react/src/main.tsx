f3a1f0e01428acec35cdddd8468f2f69d0acb288
tanyaP0405 <139056442+tanyaP0405@users.noreply.github.com>
2026-08-14 02:30:07 +0530

    Delete ClincAI-main/ClincAI-main directory

--- ClincAI-main/ClincAI-main/ClinAI/frontend-react/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { EnterpriseWorkspace } from './EnterpriseWorkspace'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <EnterpriseWorkspace />
-  </StrictMode>,
-)
